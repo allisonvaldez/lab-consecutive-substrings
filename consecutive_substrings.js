aa6ca0a100a74c527eb82d5d74a3c0081ade8056
allisonvaldez <alleyvaldez1@outlook.com>
2026-03-21 20:30:51 -0700

    finished

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,28 +1,27 @@
-// Task 3: Coding and Debugging
+// Create the function and initialize the empty substring
 function consecutiveSubstrings(string) {
   let substrings = [];
 
-  /*
-  Task 4: Determining Big O looping elements.
-  */
-  for
-  // Task 5: Comment and Document
-
-
-  // Create a new file called solution.txt.
-  // Within this file, add a short description of the solution and how it works.
-  // Add the time complexity of the solution.
+  // Looping elements.
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      substrings.push(string.slice(i, j +1));
+    }
+}
+return substrings;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
-
   console.log("");
-
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  // Test 1:  String with a space 
+  console.log("Expecting: ['a', 'a ', 'a b', ' ', ' b', 'b']");
+  console.log("=>", consecutiveSubstrings('a b'));
+  console.log("");
 }
 
 module.exports = consecutiveSubstrings;
